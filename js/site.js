1b7824657c332ce50ae2f14de1fb4604ae677de2
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2021-09-28 07:20:06 -0500

    Brand application and write logic

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let returnObj = isPalindrome(userString);
+    displayMessage(returnObj);
+}
+
+function isPalindrome(userString) {
+    let returnObj = {};
+    let regex = /[^a-z0-9]/gi;
+
+    userString = userString.toLowerCase();
+    userString = userString.replace(regex, "");
+
+    let revString = userString.split("").reverse().join("");
+
+    if (revString == userString) {
+        returnObj.msg = "Well Done! You entered a Palindrome!"
+    }
+    else {
+        returnObj.msg = "Sorry! You did not enter a Palindrome!"
+    }
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+}
+
+
+function displayMessage(returnObj) {
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
